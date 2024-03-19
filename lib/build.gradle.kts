d61eba37935cd0cff60fa5a6714727c112735eca
zjun <565061763@qq.com>
2024-03-19 17:18:30 +0800

    1.1.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "event"
-val libVersionName = "1.0.0"
+val libVersionName = "1.1.0"
 
 android {
     namespace = "com.sd.lib.event"
