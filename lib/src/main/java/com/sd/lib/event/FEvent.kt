187e1cea7f6ef2eaabc0c2dda3dc1c120d9956ed
zjun <565061763@qq.com>
2024-10-18 03:47:14 +0800

    mutableMapOf

--- lib/src/main/java/com/sd/lib/event/FEvent.kt
+++ lib/src/main/java/com/sd/lib/event/FEvent.kt
@@ -11,7 +11,7 @@ import java.lang.ref.WeakReference
 
 object FEvent {
    private val _scope = MainScope()
-   private val _flows: MutableMap<Class<*>, WeakRef<MutableSharedFlow<*>>> = hashMapOf()
+   private val _flows: MutableMap<Class<*>, WeakRef<MutableSharedFlow<*>>> = mutableMapOf()
    private val _refQueue = ReferenceQueue<Any>()
 
    var isDebug = false
