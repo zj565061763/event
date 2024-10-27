a761b6a5a5cfc3de6f1d50a9ed128f411e956de9
zjun <565061763@qq.com>
2024-10-28 01:11:48 +0800

    test

--- app/src/test/java/com/sd/demo/event/EventTest.kt
+++ app/src/test/java/com/sd/demo/event/EventTest.kt
@@ -1,15 +1,49 @@
 package com.sd.demo.event
 
+import com.sd.lib.event.FEvent
+import com.sd.lib.event.fEvent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class EventTest {
    @get:Rule
    val mainDispatcherRule = MainDispatcherRule()
 
    @Test
    fun `test event`() = runTest {
+      val count = AtomicInteger()
 
+      val job = launch {
+         fEvent<TestEvent> {
+            count.incrementAndGet()
+         }
+      }
+
+      launch {
+         fEvent<TestEvent> {
+            count.incrementAndGet()
+         }
+      }
+
+      advanceUntilIdle()
+
+      FEvent.post(TestEvent())
+      advanceUntilIdle()
+      assertEquals(2, count.get())
+
+      job.cancelAndJoin()
+      FEvent.post(TestEvent())
+      advanceUntilIdle()
+      assertEquals(3, count.get())
    }
 }
+
+private class TestEvent
