f39cf9667dcc15cbdaf2388693cc079d00e5c86b
zjun <565061763@qq.com>
2024-10-28 02:01:01 +0800

    FKeyedEvent

---
+++ lib/src/main/java/com/sd/lib/event/FKeyedEvent.kt
@@ -0,0 +1,58 @@
+package com.sd.lib.event
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class FKeyedEvent<T> {
+   private val _flows: MutableMap<String, FlowHolder<T>> = mutableMapOf()
+   private val _dispatcher = runCatching { Dispatchers.Main.immediate }.getOrDefault(Dispatchers.Main)
+   private val _scope = CoroutineScope(SupervisorJob() + _dispatcher)
+
+   fun emit(key: String, event: T) {
+      _scope.launch {
+         val holder = _flows.getOrPut(key) { FlowHolder() }
+         holder.releaseAble = false
+         holder.flow.emit(event)
+      }
+   }
+
+   fun release(key: String) {
+      _scope.launch {
+         _flows[key]?.let { holder ->
+            holder.releaseAble = true
+            holder.release(key)
+         }
+      }
+   }
+
+   suspend fun collect(
+      key: String,
+      block: suspend (T) -> Unit,
+   ) {
+      withContext(_dispatcher) {
+         val holder = _flows.getOrPut(key) { FlowHolder() }
+         try {
+            holder.flow.collect {
+               block(it)
+            }
+         } finally {
+            holder.release(key)
+         }
+      }
+   }
+
+   private fun FlowHolder<T>.release(key: String) {
+      if (releaseAble && flow.subscriptionCount.value == 0) {
+         _flows.remove(key)
+      }
+   }
+
+   private class FlowHolder<T> {
+      val flow: MutableSharedFlow<T> = MutableSharedFlow()
+      var releaseAble: Boolean = false
+   }
+}
